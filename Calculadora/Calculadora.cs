da4663c9ee58346cb4df092f1eead6bcc48a43c0
Daniel-Pina <daniel.pina@outlook.com>
2023-09-11 21:00:15 -0300

    1.1

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -11,22 +11,38 @@ namespace Calculadora
                 case '+': operacao.resultado= soma(operacao);break;
                 case '-': operacao.resultado = subtracao(operacao);break;
                 case '*': operacao.resultado = multiplicacao(operacao);break;
+                /* Incluindo a operação de divisão */
+                case '/': operacao.resultado = divisao(operacao);break;
                 default: operacao.resultado = 0; break;
             }
             return operacao;
         }
-        public int soma(Operacoes operacao)
+        /* A penúltima linha não está sendo calculada cooretamente pois ocorre um overflow do tipo int
+           Por isso, os tipos int foram substituídos por long nas operações */
+        public long soma(Operacoes operacao)
         {
-            return operacao.valorA + operacao.valorB;
+            return (long) operacao.valorA + operacao.valorB;
         }
-        public int subtracao(Operacoes operacao)
+        public long subtracao(Operacoes operacao)
         {
-            return operacao.valorA - operacao.valorB;
+            return (long) operacao.valorA - operacao.valorB;
         }
-        public int multiplicacao(Operacoes operacao)
+        public long multiplicacao(Operacoes operacao)
         {
-            return operacao.valorA * operacao.valorB;
+            return (long) operacao.valorA * operacao.valorB;
         }
+
+        /* Implementa a operação de divisão, considerando casos especiais */
+        public decimal divisao(Operacoes operacao)
+        {
+            if (operacao.valorB == 0) {
+                return -1;
+            }
+            else {
+                return operacao.valorA / operacao.valorB;
+            }
+        }
+        
        
     }
 }
--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,27 +6,65 @@ namespace Calculadora
 {
     class Program
     {
+        /* Função criada para imprimir as operações restantes na fila */
+        public void imprime_fila(Queue<Operacoes> filaOperacoes)
+        {
+            if (filaOperacoes.Count > 0) {
+                Console.Write("\t• Operações restantes:\t");
+                foreach (Operacoes operacao in filaOperacoes) {
+
+                    Console.Write("[ {0} {1} {2} ]  ", operacao.valorA,operacao.operador,operacao.valorB);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        /* Função criada para imprimir os resultados na pilha (ordem inversa dos cálculos) */
+        public void imprime_stack(Stack<decimal> pilhaResultados)
+        {
+            if (pilhaResultados.Count > 0) {
+                Console.Write("\nRESULTADOS OBTIDOS:  ");
+                foreach (decimal resultado in pilhaResultados) {
+
+                    Console.Write("{0}, ", resultado);
+                }
+                Console.WriteLine("\n");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+
             Queue<Operacoes> filaOperacoes = new Queue<Operacoes>();
             filaOperacoes.Enqueue(new Operacoes { valorA = 2, valorB = 3, operador = '+' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 14, valorB = 8, operador = '-' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 5, valorB = 6, operador = '*' });
             filaOperacoes.Enqueue(new Operacoes { valorA = 2147483647, valorB = 2, operador = '+' });
-            filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' }); //Implemente o calculo de divisao
+            filaOperacoes.Enqueue(new Operacoes { valorA = 18, valorB = 3, operador = '/' });
+
+            /* Pilha (Stack) criada para armazenar os resultados das operações */
+            Stack<decimal> pilhaResultados = new Stack<decimal>();
 
             Calculadora calculadora = new Calculadora();
-
             
-            while (filaOperacoes.Count >= 0)
+            //while (filaOperacoes.Count >= 0)
+            while (filaOperacoes.Count > 0)
+            /* Essa mudança evita que seja feita uma iteração do while com a fila vazia, o que gera erro */
             {
-                Operacoes operacao = filaOperacoes.Peek();
+                //Operacoes operacao = filaOperacoes.Peek();
+                Operacoes operacao = filaOperacoes.Dequeue();
+                /* Essa mudança garante que a operação seja removida da fila após ser calculada,
+                impedindo que o programa entre em loop infinito */
+
                 calculadora.calcular(operacao);
+                pilhaResultados.Push(operacao.resultado);
                 Console.WriteLine("{0} {1} {2} = {3}", operacao.valorA,operacao.operador,operacao.valorB, operacao.resultado);
+                p.imprime_fila(filaOperacoes);
             }
 
-          
-           
+            p.imprime_stack(pilhaResultados);
+
         }
     }
 }
